295fe4704a974b5a6615a4114b5dc8bfc4d0c25a
mostafa bukhadra <mvxizk@gmail.com>
2024-03-08 21:50:32 +0200

    ok

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""base class module"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """base model class
+    """
+
+    def __init__(self):
+        """initializing some instances attributes
+        """
+        self.id = uuid.uuid4()
+        self.id = str(self.id)
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """string representation of an object
+        """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute \
+                updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all \
+                keys/values of __dict__ of the instance
+        """
+        my_dic = {}
+        my_dic["__class__"] = self.__class__.__name__
+        for key in self.__dict__.keys():
+            if self.__dict__[key]:
+                if key == "created_at" or key == "updated_at":
+                    formating = self.__dict__[key].strftime("%Y-%m-%dT%H:%M:%S.%f")
+                    my_dic[key] = formating
+                else:
+                    my_dic[key] = self.__dict__[key]
+        return my_dic
