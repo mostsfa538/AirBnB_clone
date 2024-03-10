9651589f8f4eb5f048c9a3b6cccc998c35197492
mostafa bukhadra <mvxizk@gmail.com>
2024-03-10 20:51:04 +0200

    ok

--- console.py
+++ console.py
@@ -9,19 +9,28 @@ class HBNBCommand(cmd.Cmd):
     """manual cmd
     """
     prompt = "(hbnb) "
+
+    def emptyline(self):
+        pass
+
+    def help_emptyline(self):
+        sys.stdout.write("doing nothing if you press enter when emptyline\n")
+
     def do_EOF(self, line):
         """ending the console session
         """
         return True
+
+    def help_EOF(self):
+        sys.stdout.write("[EOF] ending the program ctrl + D in linux\n")
 
     def do_quit(self, line):
         """quitting the console session
         """
         return True
 
-    def do_emptyline(self, line):
-        pass
-        
+    def help_quit(self):
+        sys.stdout.write("quiting the program by typing <quit>")
 
 
 if __name__ == '__main__':
