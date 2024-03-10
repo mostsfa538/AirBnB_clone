5da0ac2f33c99758220e9cffe00b7b0a2c7b3c19
mostafa bukhadra <mvxizk@gmail.com>
2024-03-10 19:48:46 +0200

    ok

--- tests/test_engines/test_file_storage.py
+++ tests/test_engines/test_file_storage.py
@@ -2,9 +2,20 @@
 """testing file_storage engine"""
 
 import unittest
+from models.engine.file_storage import FileStorage
+from models import storage
 
 
-class Temp_name(unittest.TestCase):
+class test_storage(unittest.TestCase):
     """temp doc class
     """
-    pass
+
+    def test_init(self):
+        all_objs = storage.all()
+        for obj_id in all_objs.keys():
+            obj = all_objs[obj_id]
+        assertAlmostEqual(obj, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-"""testing base class module"""
+"""testing file_storage engine"""
 
 import unittest
+from models.base_model import BaseModel
 
 
-class Temp_name(unittest.TestCase):
-    """Temp doc
+class test_base(unittest.TestCase):
+    """temp doc class
     """
-    pass
+
+    def test_init(self):
+        result = """[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {
+        'my_number': 89, 'name': 'My First Model', 'updated_at'
+        : datetime.datetime(2017, 9, 28, 21, 5, 54, 119434), 'id'
+        : 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at'
+        : datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)}
+        """
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        assertAlmostEqual(my_model.__str__(), result)
+
+
+if __name__ == "__main__":
+    unittest.main()
